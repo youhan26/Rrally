135b7496a84bef244f1f6bbd44b6e2f6b944061e
YouHan26 <youhan26@gmail.com>
2016-04-23 12:58:07 +0800

    complete story list page

--- app/story/storyList.js
+++ app/story/storyList.js
@@ -6,25 +6,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Firebase = require('firebase');
 
-var List = React.createClass({
-    displayName: 'list',
-    render: function () {
-        function renderLi(item) {
-            return (
-                <li key={item.key}>
-                    {item.name}
-                </li>
-            );
-        }
-
-        return (
-            <div>
-                <ul>{this.props.data.map(renderLi)}</ul>
-            </div>
-        )
-    }
-});
-
 var StoryList = React.createClass({
     getInitialState: function () {
         return {
@@ -33,38 +14,68 @@ var StoryList = React.createClass({
     },
     componentWillMount: function () {
         this.firebaseRef = new Firebase('https://mimikiyru.firebaseio.com/story');
-        this.firebaseRef.on("child_added", function (data) {
-            this.state.items.push(data.val());
+        this.firebaseRef.on("child_added", function (snap) {
+            this.state.items.push(snap.val());
             this.setState({
                 items: this.state.items
             });
         }.bind(this));
-        //TODO 'child_changed', 'child_removed', 'child_moved'
+        this.firebaseRef.on('child_changed', function (snap) {
+            var data = snap.val();
+            if (data.id) {
+                var list = this.state.items;
+                for (var i in list) {
+                    if (list[i].id === data.id) {
+                        list[i] = data;
+                        break;
+                    }
+                }
+                this.setState({
+                    items: this.state.items
+                });
+            }
+        }.bind(this));
+        this.firebaseRef.on('child_removed', function (snap) {
+            var data = snap.val();
+            if (data.id) {
+                var list = this.state.items;
+                for (var i in list) {
+                    if (list[i].id === data.id) {
+                        list.splice(i, 1);
+                        break;
+                    }
+                }
+                this.setState({
+                    items: this.state.items
+                });
+            }
+        }.bind(this));
     },
     componentWillUnmount: function () {
         this.firebaseRef.off();
     },
-    // submit: function (e) {
-    //     e.preventDefault();
-    //     var date = new Date();
-    //     this.firebaseRef.push({
-    //         name: this.state.text,
-    //         key: new Date().getTime().toString()
-    //     });
-    //     this.state.text = '';
-    //     this.setState({
-    //         text: this.state.text
-    //     });
-    // },
-    // textChange: function (e) {
-    //     this.setState({
-    //         text: e.target.value
-    //     });
-    // },
     render: function () {
+        function renderLi(item) {
+            return (
+                <li key={item.id}>
+                    <section>
+                        <h3>{item.basic.name}</h3>
+                        <h4>Desc:</h4>
+                        {item.basic.desc}
+                        <h4>Note :</h4>
+                        {item.basic.note}
+
+                        <label>Plan Est:</label>{item.status.planEst}
+                        <label>Task Est:</label>{item.status.taskEst}
+                        <label>TODO:</label>{item.status.todo}
+                    </section>
+                </li>
+            );
+        }
+
         return (
             <div>
-                <List data={this.state.items}></List>
+                <ul>{this.state.items.map(renderLi)}</ul>
             </div>
         )
     }
@@ -72,6 +83,7 @@ var StoryList = React.createClass({
 
 
 var el = document.getElementById('storyList');
+
 if (el) {
     ReactDOM.render(React.createElement(StoryList, null), el);
 }
