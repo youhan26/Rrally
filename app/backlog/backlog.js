1eaeda0e662d3e8b64bd6a1bd6151fc73d7c8e62
YouHan26 <youhan26@gmail.com>
2016-05-09 16:36:29 +0800

    update backlog

--- app/backlog/backlog.js
+++ app/backlog/backlog.js
@@ -5,10 +5,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Firebase = require('firebase');
-var bs = require('react-bootstrap');
 var constant = require('./../common/config');
-var Button = bs.Button;
-var Well = bs.Well;
+
+var MD = require('material-ui');
+var Table = MD.Table;
+var TableHeader = MD.TableHeader;
+var TableRow = MD.TableRow;
+var TableHeaderColumn = MD.TableHeaderColumn;
+var TableRowColumn = MD.TableRowColumn;
+var TableBody = MD.TableBody;
+var RaisedButton = MD.RaisedButton;
+
 
 var StoryList = React.createClass({
     getInitialState: function () {
@@ -18,7 +25,41 @@ var StoryList = React.createClass({
     },
     componentWillMount: function () {
         this.firebaseRef = new Firebase(constant.story);
-        this.loadData();
+        this.projectRef = new Firebase(constant.host + '/project');
+        this.memberRef = new Firebase(constant.host + '/member');
+        this.style = {
+            width: '35%'
+        };
+        this.style2 = {
+            width: '15%'
+        };
+        this.loadProjectData(true);
+    },
+    loadProjectData: function (loadData) {
+        this.projectRef.once('value', function (snap) {
+            this.projects = snap.val();
+            this.memberRef.once('value', function (snap) {
+                this.members = snap.val();
+                if (loadData) {
+                    this.loadData();
+                }
+            }.bind(this));
+        }.bind(this));
+    },
+    getProjectById: function (id) {
+        return this.getDataById(this.projects, id);
+    },
+    getPmById: function (id) {
+        return this.getDataById(this.members, id);
+    },
+    getDataById: function (list, id) {
+        if (id) {
+            for (var i in list) {
+                if (list[i].id === parseInt(id)) {
+                    return list[i].name;
+                }
+            }
+        }
     },
     loadData: function () {
         this.firebaseRef.once('value', function (snap) {
@@ -51,6 +92,7 @@ var StoryList = React.createClass({
     },
     componentWillUnmount: function () {
         this.firebaseRef.off();
+        this.projectRef.off();
     },
     up: function (data) {
         var list = this.state.items;
@@ -86,43 +128,57 @@ var StoryList = React.createClass({
     },
     renderLi: function (item) {
         return (
-            <StoryItem story={item} key={item.id} up={this.up} down={this.down}>
-            </StoryItem>
+            <TableRow hoverable={true} key={item.id}>
+                <TableRowColumn>
+                    <a href={'../story/story.html?id=' + item.storyId} target="_blank">
+                        {item.storyId}
+                    </a>
+                </TableRowColumn>
+                <TableRowColumn style={this.style}>{item.basic.name}</TableRowColumn>
+                <TableRowColumn style={this.style2}>{item.status.planEst}</TableRowColumn>
+                <TableRowColumn>
+                    {this.getProjectById(item.schedule.project) || '无'}
+                    / {this.getPmById(item.basic.pm) || '无'}
+                </TableRowColumn>
+                <Operation story={item} up={this.up} down={this.down}></Operation>
+            </TableRow>
         );
     },
     render: function () {
         return (
             <div>
-                {this.state.items.map(this.renderLi)}
+                <Table>
+                    <TableHeader displaySelectAll={false}>
+                        <TableRow>
+                            <TableHeaderColumn>Story ID</TableHeaderColumn>
+                            <TableHeaderColumn style={this.style}>Story Name</TableHeaderColumn>
+                            <TableHeaderColumn style={this.style2}>Plan Est</TableHeaderColumn>
+                            <TableHeaderColumn>Project / PM Owner</TableHeaderColumn>
+                            <TableHeaderColumn>Operation</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody displayRowCheckbox={false} stripedRows={true}>
+                        {this.state.items.map(this.renderLi)}
+                    </TableBody>
+                </Table>
             </div>
         )
     }
 });
 
-var StoryItem = React.createClass({
-    up: function () {
-        this.props.up(this.props.story);
-    },
+var Operation = React.createClass({
     down: function () {
         this.props.down(this.props.story);
     },
+    up: function () {
+        this.props.up(this.props.story);
+    },
     render: function () {
         return (
-            <div>
-                <Well className="storyItem">
-                    <a href={'../story/story.html?id=' + this.props.story.storyId} target="_blank">
-                        <Button bsStyle="default" onClick={this.goEditPage}>
-                            {this.props.story.storyId}
-                        </Button>
-                    </a>
-                    &nbsp;&nbsp;
-                    {this.props.story.basic.name}
-                    <div className="right">
-                        <Button onClick={this.up}>&#8679;</Button>
-                        <Button onClick={this.down}>&#8681;</Button>
-                    </div>
-                </Well>
-            </div>
+            <TableRowColumn>
+                <RaisedButton label="&#8679;" onClick={this.up}/>
+                <RaisedButton label="&#8681;" secondary={true} onClick={this.down}/>
+            </TableRowColumn>
         )
     }
 });
--- config/backlog.js
+++ config/backlog.js
@@ -10,8 +10,9 @@ require('./../node_modules/react-bootstrap/dist/react-bootstrap.min.js');
 require('./../node_modules/firebase/lib/firebase-web');
 // require('./../node_modules/reactfire/dist/reactfire.min.js');
 
-//marked
-// require('./../node_modules/marked/marked.min.js');
+//material ui
+require('./../node_modules/material-ui');
+require("./../node_modules/react-tap-event-plugin");
 
 //app.js
 // require('./app/js/app');
